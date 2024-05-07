b96be307466826f9d9ae969f6cc22133af78d9f7
nisol <nisolmedinaue@gmail.com>
2024-05-07 16:53:06 +0200

    iteration1done

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -6,12 +6,36 @@ function ProductListPage() {
   // but you should use it to store the response from the Fake Store API (the list of products).
   const [products, setProducts] = useState([]);
 
+  useState(()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then((response) => response.json())
+    .then((data)=>setProducts(data))
+    .catch((error)=> console.log(error));
+    
+  },[])
+
+  ;
+
+
   // To fetch the list of products, set up an effect with the `useEffect` hook:
 
 
   return (
     <div className="ProductListPage">
       {/* Render list of products here */}
+      {products.map((product)=>{
+        return(
+          <div key={product.id}className="list">
+            <img className="imagen" src={product.image}alt={`${product.images}image`}/>
+            <p>{product.title}</p>
+            <p>{product.price}</p>
+            <p>{product.category}</p>
+            <p>{product.description}</p>
+          </div>
+        )
+      })}
+
+
     </div>
   );
 }
